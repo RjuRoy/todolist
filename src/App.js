b852cf7f1703300d63d703dcc566a0005cdc3cfa
RjuRoy <roy.sagar9@gmail.com>
2020-02-27 07:24:36 +0530

    making changes in appjs

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import uuid from 'uuid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ToDoInput from './components/ToDoInput';
 import TodoList from './components/TodoList';
-
+//how to commit things in git
  class App extends Component {
   render() {
     return (
