b01d29b7c14fb5c687cc378c45ec5ea51a144a46
RjuRoy <roy.sagar9@gmail.com>
2020-02-27 08:16:14 +0530

    changes made is app js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React, { Component } from 'react'
-import uuid from 'uuid';
+import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ToDoInput from './components/ToDoInput';
 import TodoList from './components/TodoList';
 //how to commit things in git
  class App extends Component {
+   state = {
+     items:[
+       {id:1, title:'wake up'},
+       {id:2, title:'eat breakfast'}
+      ],
+      id: uuidv4(),
+      item:'',
+      editItem:false
+   };
+
+   handleChange = (e) =>{
+     console.log('handle change');
+   }
+
+   handleSubmit = (e) => {
+     console.log("handle submit");
+   }
+
+   clearList = () => {
+    console.log("clear list");
+  }
+  handleDelete = (id) => {
+    console.log(`handle delete ${id}`);
+  }
+  handleEdit = (id) => {
+    console.log(`handle edit ${id}`);
+  }
+
+
   render() {
+    console.log(this.state);
     return (
      <div>
        <div className="container">
